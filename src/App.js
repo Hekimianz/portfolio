83ab1f8a7a2b4d6e02e0c06fd16da5cd32aa7481
Aram Hekimian <aramhek121@gmail.com>
2023-05-15 14:51:07 -0600

    Implemented Contact section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import About from "./components/About.js";
 import Work from "./components/Work";
 import Skills from "./components/Skills";
+import Contact from "./components/Contact";
 export default function App() {
   return (
     <main id="main-cont">
       <About />
       <Work />
       <Skills />
+      <Contact />
     </main>
   );
 }
