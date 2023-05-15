2ccce0a5af05330fdbdc72fc89ae6dba4515a937
Aram Hekimian <aramhek121@gmail.com>
2023-05-15 14:16:17 -0600

    Made Skills section responsive and added styling

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,87 +3,118 @@ export default function Skills() {
     <section id="skills-cont">
       <h2 className="cont-title">Skills</h2>
       <div id="skills-bar">
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg"
-          alt="HTML"
-          className="skill-icon"
-        />
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg"
-          alt="CSS"
-          className="skill-icon"
-        />
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"
-          alt="JavaScript"
-          className="skill-icon"
-        />
-
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
-          alt="React"
-          className="skill-icon"
-        />
-
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
-          alt="Bootstrap"
-          className="skill-icon"
-        />
-
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg"
-          alt="NPM"
-          className="skill-icon"
-        />
-
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-original.svg"
-          alt="Webpack"
-          className="skill-icon"
-        />
-
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg"
-          alt="C"
-          className="skill-icon"
-        />
-
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg"
-          alt="git"
-          className="skill-icon"
-        />
-
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg"
-          alt="jest"
-          className="skill-icon"
-        />
-
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
-          alt="VS Code"
-          className="skill-icon"
-        />
-
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-line.svg"
-          alt="Photoshop"
-          className="skill-icon"
-        />
-
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/illustrator/illustrator-line.svg"
-          alt="Illustrator"
-          className="skill-icon"
-        />
-
-        <img
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"
-          alt="Figma"
-          className="skill-icon"
-        />
+        <div className="skillCont">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg"
+            alt="HTML"
+            className="skill-icon"
+          />
+          <h3 className="skill-title">HTML</h3>
+        </div>
+        <div className="skillCont">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg"
+            alt="CSS"
+            className="skill-icon"
+          />
+          <h3 className="skill-title">CSS</h3>
+        </div>
+        <div className="skillCont">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg"
+            alt="JavaScript"
+            className="skill-icon"
+          />
+          <h3 className="skill-title">JavaScript</h3>
+        </div>
+        <div className="skillCont">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
+            alt="React"
+            className="skill-icon"
+          />
+          <h3 className="skill-title">React</h3>
+        </div>
+        <div className="skillCont">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg"
+            alt="Bootstrap"
+            className="skill-icon"
+          />
+          <h3 className="skill-title">Bootstrap</h3>
+        </div>
+        <div className="skillCont">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg"
+            alt="NPM"
+            className="skill-icon"
+          />
+          <h3 className="skill-title">NPM</h3>
+        </div>
+        <div className="skillCont">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-original.svg"
+            alt="Webpack"
+            className="skill-icon"
+          />
+          <h3 className="skill-title">Webpack</h3>
+        </div>
+        <div className="skillCont">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg"
+            alt="C"
+            className="skill-icon"
+          />
+          <h3 className="skill-title">C</h3>
+        </div>
+        <div className="skillCont">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain-wordmark.svg"
+            alt="git"
+            className="skill-icon"
+          />
+          <h3 className="skill-title">Git</h3>
+        </div>
+        <div className="skillCont">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg"
+            alt="jest"
+            className="skill-icon"
+          />
+          <h3 className="skill-title">Jest</h3>
+        </div>
+        <div className="skillCont">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
+            alt="VS Code"
+            className="skill-icon"
+          />
+          <h3 className="skill-title">VS Code</h3>
+        </div>
+        <div className="skillCont">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/photoshop/photoshop-line.svg"
+            alt="Photoshop"
+            className="skill-icon"
+          />
+          <h3 className="skill-title">Photoshop</h3>
+        </div>
+        <div className="skillCont">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/illustrator/illustrator-line.svg"
+            alt="Illustrator"
+            className="skill-icon"
+          />
+          <h3 className="skill-title">Illustrator</h3>
+        </div>
+        <div className="skillCont">
+          <img
+            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"
+            alt="Figma"
+            className="skill-icon"
+          />
+          <h3 className="skill-title">Figma</h3>
+        </div>
       </div>
     </section>
   );
