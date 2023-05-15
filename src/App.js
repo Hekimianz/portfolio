bab6d9934b9e8633c761bfd4d9c2b593ca3d67af
Aram Hekimian <aramhek121@gmail.com>
2023-05-15 12:18:31 -0600

    Made My work section responsive

--- src/App.js
+++ src/App.js
@@ -70,48 +70,50 @@ function Work() {
   return (
     <section id="work-cont">
       <h2 className="cont-title">My work</h2>
-      <Project
-        name="WINDi"
-        desc="Weather lookup app. Built using OpenWeather API."
-        img={windi}
-        git="https://github.com/Hekimianz/windi"
-        live="https://hekimianz.github.io/windi/"
-      />
-      <Project
-        name="Memorón"
-        desc="Pokémon themed memory game."
-        img={memoron}
-        git="https://github.com/Hekimianz/react-memory-card"
-        live="https://hekimianz.github.io/react-memory-card/"
-      />
-      <Project
-        name="Battleship"
-        desc="Classic Battleship board game."
-        img={battleship}
-        git="https://github.com/Hekimianz/battleship"
-        live="https://hekimianz.github.io/battleship/"
-      />
-      <Project
-        name="Tenzies"
-        desc="Fast-paced dice game that involves rolling and re-rolling ten dice to match a specific number."
-        img={tenzies}
-        git="https://github.com/Hekimianz/tenzies"
-        live="https://hekimianz.github.io/tenzies/"
-      />{" "}
-      <Project
-        name="DOiT"
-        desc="Simple To Do App."
-        img={doit}
-        git="https://github.com/Hekimianz/ToDo"
-        live="https://hekimianz.github.io/ToDo/"
-      />
-      <Project
-        name="Library"
-        desc="App that keeps track on books you have read, or books you want to read."
-        img={library}
-        git="https://github.com/Hekimianz/library"
-        live="https://hekimianz.github.io/library/"
-      />
+      <div id="projects-cont">
+        <Project
+          name="WINDi"
+          desc="Weather lookup app. Built using OpenWeather API."
+          img={windi}
+          git="https://github.com/Hekimianz/windi"
+          live="https://hekimianz.github.io/windi/"
+        />
+        <Project
+          name="Memorón"
+          desc="Pokémon themed memory game."
+          img={memoron}
+          git="https://github.com/Hekimianz/react-memory-card"
+          live="https://hekimianz.github.io/react-memory-card/"
+        />
+        <Project
+          name="Battleship"
+          desc="Classic Battleship board game."
+          img={battleship}
+          git="https://github.com/Hekimianz/battleship"
+          live="https://hekimianz.github.io/battleship/"
+        />
+        <Project
+          name="Tenzies"
+          desc="Fast-paced dice game that involves rolling and re-rolling ten dice to match a specific number."
+          img={tenzies}
+          git="https://github.com/Hekimianz/tenzies"
+          live="https://hekimianz.github.io/tenzies/"
+        />{" "}
+        <Project
+          name="DOiT"
+          desc="Simple To Do App."
+          img={doit}
+          git="https://github.com/Hekimianz/ToDo"
+          live="https://hekimianz.github.io/ToDo/"
+        />
+        <Project
+          name="Library"
+          desc="App that keeps track on books you have read, or books you want to read."
+          img={library}
+          git="https://github.com/Hekimianz/library"
+          live="https://hekimianz.github.io/library/"
+        />
+      </div>
     </section>
   );
 }
